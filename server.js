9c978aa5d95f784cb4c6721c614d5e28d50a7785
Oleg833 <oleg.kovtun447@gmail.com>
2023-07-05 01:44:11 +0300

    add mongoose

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
 
-app.listen(3003, () => {
-  console.log("Server running. Use our API on port: 3003");
+require("dotenv").config();
+const { PORT, DB_HOST } = process.env;
+
+app.listen(PORT, () => {
+  mongoose
+    .connect(DB_HOST)
+    .then(() => {
+      app.listen(3000);
+      console.log(`Server running. Use our API on port: ${PORT}.`);
+    })
+    .catch((error) => {
+      console.log(error.message);
+      process.exit(1);
+    });
 });
