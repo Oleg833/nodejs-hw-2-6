9fcae1f14a211d07e2f62193f9192ded80fac244
Oleg833 <oleg.kovtun447@gmail.com>
2023-07-19 14:59:54 +0300

    clear

--- app.js
+++ app.js
@@ -4,38 +4,6 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 
-const fs = require("fs/promises");
-const { upload } = require("./middlewares");
-const path = require("path");
-
-// const tempDir = path.join(__dirname, "temp");
-// const multer = require("multer");
-// const multerConfig = multer.diskStorage({
-//   destination: tempDir,
-//   filename: (req, file, cb) => {
-//     cb(null, file.originalname);
-//   },
-// });
-// const upload = multer({
-//   storage: multerConfig,
-// });
-// upload.fields([{ name: "cover", maxCount: 100 }, { name: "subcover", maxCount: 2 }]);
-// upload.array("files", 8);
-
-const avatarsDir = path.join(__dirname, "public", "avatars");
-
-app.post("/api/avatars", upload.single("cover"), async (req, res) => {
-  console.log(req.query);
-  console.log(req.body);
-  console.log(req.file);
-  const { path: tempUpload, originalname } = req.file;
-  const resultUpload = path.join(avatarsDir, originalname);
-  await fs.rename(tempUpload, resultUpload);
-  const avatar = path.join("avatars", originalname);
-
-  res.status(201).json(avatar);
-});
-
 const authRouter = require("./routes/api/auth");
 
 const contactsRouter = require("./routes/api/contacts");
